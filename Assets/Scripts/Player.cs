8394f895621d673d1ed0da8ce03709470a7aa13a
devseylor <vasitovm@gmail.com>
2021-08-26 16:42:01 +0500

    Создал проект!

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    //Configuration parameters 
+    [Header("Player")]
+    [SerializeField] float moveSpeed = 10f;
+    [SerializeField] float padding = 1f;
+    [SerializeField] int health = 200;
+    [SerializeField] AudioClip deathSFX;
+    [SerializeField] [Range(0, 1)] float deathSoundVolume = 0.65f;
+    [SerializeField] [Range(0, 1)] float shootSoundVolume = 0.15f;
+    [SerializeField] Joystick joystick;
+    bool pressed = false;
+    [Header("Project Tail")]
+    [SerializeField] GameObject laserPrefab;
+    [SerializeField] float projectailSpeed = 20f;
+    [SerializeField] float projectailFirePeriod = 0.1f;
+    
+
+    Coroutine firingCoroutine;
+
+    float xMin;
+    float xMax;
+    float yMin;
+    float yMax;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetUpMoveBounderies();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        DamageDealer damageDealer = other.gameObject.GetComponent<DamageDealer>();
+        if (!damageDealer) { return; }
+        ProcessHit(damageDealer);
+    }
+
+    private void ProcessHit(DamageDealer damageDealer)
+    {
+        health -= damageDealer.GetDamage();
+        damageDealer.Hit();
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        FindObjectOfType<Level>().LoadGameOver();
+        Destroy(gameObject); 
+        AudioSource.PlayClipAtPoint(deathSFX, Camera.main.transform.position, deathSoundVolume);
+    }
+    public int GetHealth()
+    {
+        return health;
+    }
+
+    public void FireUp()
+    {
+        
+            Debug.Log("True");
+            firingCoroutine = StartCoroutine(FireContinuosly());        
+       
+        
+    }
+    public void FireDown()
+    {
+        Debug.Log("False");
+        StopCoroutine(firingCoroutine);
+    }
+
+    IEnumerator FireContinuosly()
+    {
+        while (true)
+        {
+            GameObject laser = Instantiate(laserPrefab, transform.position, Quaternion.identity) as GameObject;
+            laser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, projectailSpeed);
+            yield return new WaitForSeconds(projectailFirePeriod);
+        }
+    }
+
+    private void Move()
+    {
+        float deltaX = joystick.Horizontal * Time.deltaTime * moveSpeed;
+        float deltaY = joystick.Vertical * Time.deltaTime * moveSpeed;
+
+        var newXPos = Mathf.Clamp(transform.position.x + deltaX, xMin, xMax);
+        var newYPos = Mathf.Clamp(transform.position.y + deltaY, yMin, yMax); 
+        transform.position = new Vector2(newXPos, newYPos);
+    }
+
+    private void SetUpMoveBounderies()
+    {
+        Camera gameCamera = Camera.main;
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + padding;
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - padding;
+        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + padding;
+        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - padding;
+    }
+}
+
